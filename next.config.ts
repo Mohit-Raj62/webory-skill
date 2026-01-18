695422edddccfb2e75f309e18cc84c7014683e14
mohit <singm2698@gmail.com>
2026-01-19 00:11:31 +0530

    feat: Configure Turbopack to load SVGs using @svgr/webpack in experimental settings.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbopack: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
